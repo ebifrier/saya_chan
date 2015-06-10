fcacb7f06a6127cdfdb9c8b99550368e3df0fd14
ebifrier <ebifrier@gmail.com>
2015-06-10 23:14:57 +0900

    プロジェクトにSAYAを定義するようにしparam_new.hを読み込むようにしました。

---
+++ src/saya_chan/param_new.h
@@ -0,0 +1,14 @@
+﻿#define DPawn            94 /*  188 */
+#define DLance          232 /*  464 */
+#define DKnight         257 /*  514 */
+#define DProPawn        523 /*  617 */
+#define DSilver         356 /*  712 */
+#define DProLance       486 /*  718 */
+#define DProKnight      497 /*  754 */
+#define DProSilver      477 /*  833 */
+#define DGold           461 /*  922 */
+#define DBishop         581 /* 1162 */
+#define DRook           659 /* 1318 */
+#define DHorse          838 /* 1419 */
+#define DDragon         939 /* 1598 */
+#define DKing         15000
